9f8219e86c9d27dd2d1dfe7437fe9be76e8c03c0
Petr Kulhanek <kulhanek@chemi.muni.cz>
2015-12-12 14:06:23 +0100

    win/unx build

--- CMakeClean.bat
+++ CMakeClean.bat
@@ -7,14 +7,7 @@ if "%MODE%"=="" (
 )
 
 if "%MODE%"=="quick" (
-	for /d /r . %%d in (CMakeFiles) do (
-		if exist "%%d" rd /s/q "%%d"
-	)
-	del /s /q /f CMakeCache.txt
-	del /s /q /f cmake_install.cmake
-	del /s /q /f Makefile
-	del /s /q /f *~
-	del /s /q /f *.obj	
+	REM nothing to be here
 )
 
 if "%MODE%"=="deep" (
@@ -24,12 +17,8 @@ if "%MODE%"=="deep" (
 	del /s /q /f CMakeCache.txt
 	del /s /q /f cmake_install.cmake
 	del /s /q /f Makefile
-	del /s /q /f *.obj
-	del /s /q /f *.a
-	del /s /q /f *.dll
-	del /s /q /f *.dll.*
-	del /s /q /f *.mod
-	for /d /r . %%d in (bin\*) do (
-		if exist "%%d" rd /s/q "%%d"
-	)
+	del /s /q /f VTK-6.3.0.*
+	rd /s/q build
+	rd /s/q src/build
+	rd /s/q src/VTK-6.3.0
 )
--- CMakeClean.sh
+++ CMakeClean.sh
@@ -7,27 +7,17 @@ fi
 
 case $MODE in
     "quick")
-        rm -rf `find . -name CMakeFiles`
-        rm -rf `find . -name CMakeCache.txt`
-        rm -rf `find . -name cmake_install.cmake`
-        rm -rf `find . -name Makefile`
-        rm -rf `find . -name '*~'`
-        rm -rf `find . -name '*.o'`
-        rm -rf `find . -name 'VTK-6.3.0*'`
+		# nothing to be here
+		echo "Nothing done ..."
     ;;
     "deep")
         rm -rf `find . -name CMakeFiles`
         rm -rf `find . -name CMakeCache.txt`
         rm -rf `find . -name cmake_install.cmake`
         rm -rf `find . -name Makefile`
-        rm -rf `find . -name '*~'`
-        rm -rf `find . -name '*.o'`
-        rm -rf `find . -name '*.a'`
-        rm -rf `find . -name '*.so'`
-        rm -rf `find . -name '*.so.*'`
-        rm -rf `find . -name '*.mod'`
         rm -rf `find . -name 'VTK-6.3.0*'`
         rm -rf build
+        rm -rf src/build
     ;;
 esac
 
